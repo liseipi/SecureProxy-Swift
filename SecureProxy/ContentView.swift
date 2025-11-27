ca7ddc2298b59275997ee2341ca9f0ea9d81e7b9
oli <oli@qq.com>
2025-11-27 16:04:08 +0800

    Initial Commit

---
+++ SecureProxy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecureProxy
+//
+//  Created by huiliu on 2025/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SecureProxy/SecureProxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureProxyApp.swift
+//  SecureProxy
+//
+//  Created by huiliu on 2025/11/27.
+//
+
+import SwiftUI
+
+@main
+struct SecureProxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
